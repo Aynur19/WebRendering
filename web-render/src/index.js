c25ef72a88594f659cdd662e0c1faf36b8007869
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2022-12-15 23:58:41 +0300

    added ball;

--- web-render/src/index.js
+++ web-render/src/index.js
@@ -13,9 +13,58 @@ let orbitControls;
 let stats = new Stats();
 
 
-// let cameraGUI = gui.addFolder('Camera');
+function addBall(scene) {
+    const textureLoader = new THREE.TextureLoader();
+    let ballGeom = new THREE.SphereGeometry(1, 128, 128);
+    let ballMat = new THREE.MeshStandardMaterial({color: 0x0000ff, roughness: 0});
+    ballMat.map = textureLoader.load('./textures/ball.png');
+    ballMat.map.wrapS = ballMat.map.wrapT = THREE.WrapAroundEnding;
+    ballMat.map.repeat.set(2, 2);
+
+    let ballMesh = new THREE.Mesh(ballGeom, ballMat);
+    ballMesh.position.set(0, 0, 2);
+    ballMesh.name = 'Ball';
+
+    let ballGUI = gui.addFolder('Ball');
+    
+    ballGUI.add(ballMesh.position, 'x', -300, 300, 1)
+    .onChange(function(newValue) {
+        ballMesh.position.setX(newValue);
+    }).name('Position X');
+    
+    ballGUI.add(ballMesh.position, 'y', -300, 300, 1)
+    .onChange(function(newValue) {
+        ballMesh.position.setY(newValue);
+    }).name('Position Y');
+    
+    ballGUI.add(ballMesh.position, 'z', -300, 300, 1)
+    .onChange(function(newValue) {
+        ballMesh.position.setZ(newValue);
+    }).name('Position Z');
+
+    ballGUI.add(ballMesh.rotation, 'x', 0, Math.PI*2, 0.01)
+    .onChange(function(newValue) {
+        ballMesh.rotateX(newValue);
+    }).name('Rotation X');
+    
+    ballGUI.add(ballMesh.rotation, 'y', 0, Math.PI*2, 0.01)
+    .onChange(function(newValue) {
+        ballMesh.rotateY(newValue);
+    }).name('Rotation Y');
+    
+    ballGUI.add(ballMesh.rotation, 'z', 0, Math.PI*2, 0.01)
+    .onChange(function(newValue) {
+        ballMesh.rotateZ(newValue);
+    }).name('Rotation Z');
 
 
+    ballGUI.add(ballMesh, 'visible')
+    .onChange(function(newValue) {
+        ballMesh.visible = newValue;
+    }).name(`${ballMesh.name} is visible`);
+
+    scene.add(ballMesh);
+}
 
 function addStats() {
     stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
@@ -99,6 +148,36 @@ function sceneInit() {
     scene.add(light);
 
     addPlane(scene);
+    addBall(scene);
+}
+
+function addMap(map, scale) {
+    map.wrapS = map.wrapT = THREE.RepeatWrapping;
+    map.repeat.set(scale.x, scale.y);
+}
+
+function getFieldMat(textureLoader, textScale) {
+    const fieldMat = new THREE.MeshStandardMaterial();
+
+    fieldMat.map = textureLoader.load('./textures/Grass_1K/Grass_1K_Albedo.png');
+    addMap(fieldMat.map, textScale);
+    
+    fieldMat.aoMap = textureLoader.load('./textures/Grass_1K/Grass_1K_AO.png');
+    addMap(fieldMat.aoMap, textScale);
+    
+    fieldMat.displacementMap = textureLoader.load('./textures/Grass_1K/Grass_1K_Height.png');
+    addMap(fieldMat.displacementMap, textScale);
+
+    fieldMat.normalMap = textureLoader.load('./textures/Grass_1K/Grass_1K_Normal.png');
+    addMap(fieldMat.normalMap, textScale);
+
+    fieldMat.roughnessMap = textureLoader.load('./textures/Grass_1K/Grass_1K_Roughness.png');
+    addMap(fieldMat.roughnessMap, textScale);
+    
+    fieldMat.metalnessMap = textureLoader.load('./textures/Grass_1K/Grass_1K_Metallic.png');
+    addMap(fieldMat.metalnessMap, textScale);
+
+    return fieldMat;
 }
 
 function addPlane(scene) {
@@ -108,14 +187,15 @@ function addPlane(scene) {
     };
 
     let plane = new THREE.PlaneGeometry(planeSize.x, planeSize.y);
-    let planeMat = new THREE.MeshPhongMaterial({
-        color: Math.random() * 0xf2f2f2,
-        flatShading: true,
-        vertexColors: THREE.VertexColors,
-        transparent: true,
-        opacity: 0.7
-    });
-    let footballFieldMesh = new THREE.Mesh(plane, planeMat);
+
+    const textScale = {
+        x: 10, 
+        y: 5
+    };
+    const textureLoader = new THREE.TextureLoader();
+    const fieldMat = getFieldMat(textureLoader, textScale);
+
+    let footballFieldMesh = new THREE.Mesh(plane, fieldMat);
     let footballFieldGUI = gui.addFolder('Football Field');
     footballFieldGUI.add(footballFieldMesh.position, 'x', -300, 300, 1)
     .onChange(function(newValue) {
@@ -160,6 +240,52 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     render()
 }
+
+
+
+
+// let SprayBrush = (function () {
+//     function SprayBrush(radius, color) {
+//         this.radius = radius;
+//         this.color = color;
+//         this._canvasContext = null;
+//         this._density = 70;
+//     };
+
+//     SprayBrush.prototype.startStroke = function (canvas, position) {
+//         this._canvasContext = canvas.getContext('2d');
+//         this._canvasContext.beginPath();
+//         this._canvasContext.save(); // Assumption: nobody else will call this until the stroke is finished
+//         this._canvasContext.fillStyle = this.color;
+//     };
+
+//     SprayBrush.prototype.continueStoke = function (position) {
+//         if (this._canvasContext) {
+//             for (var i = this._density; i--;) {
+//                 var dotRadius = Chameleon.getRandomFloat(0, this.radius);
+//                 var angle = Chameleon.getRandomFloat(0, Math.PI * 2);
+//                 var dotWidth = Chameleon.getRandomFloat(1, 2);
+//                 this._canvasContext.globalAlpha = Math.random();
+//                 this._canvasContext.fillRect(position.x + dotRadius * Math.cos(angle), position.y + dotRadius * Math.sin(angle), dotWidth, dotWidth);
+//             }
+//         }
+//     };
+
+//     SprayBrush.prototype.finishStroke = function () {
+//         if (this._canvasContext) {
+//             this._canvasContext.restore();
+//             this._canvasContext = null;
+//         }
+//     };
+
+//     return SprayBrush;
+// })();
+
+// let SB = new SprayBrush(5, '#000000');
+
+
+
+
 
 /**
  * Animate
--- web-render/webpack.config.js
+++ web-render/webpack.config.js
@@ -1,5 +1,13 @@
 //Ensure html-webpack-plugin is pre-installed via npm.
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const portFinderSync = require('portfinder-sync')
+const path = require('path');
+
+const infoColor = (_message) =>
+{
+    return `\u001b[1m\u001b[34m${_message}\u001b[39m\u001b[22m`
+}
 
 module.exports = {
   module: {
@@ -13,12 +21,66 @@ module.exports = {
           }
         ]
       },
+
+      // Images
+      {
+        test: /\.(jpg|png|gif|svg|tif)$/,
+        type: 'asset/resource',
+        generator:
+        {
+          filename: 'assets/images/[hash][ext]'
+        }
+      }
     ]
   },
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, './static') }
+      ]
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: "./index.html"
     }),
-  ]
+  ],
+  stats: 'errors-warnings',
+  mode: 'development',
+  infrastructureLogging:
+  {
+    level: 'warn',
+  },
+  devServer:
+  {
+    host: 'local-ip',
+    port: portFinderSync.getPort(8080),
+    open: true,
+    https: false,
+    allowedHosts: 'all',
+    hot: false,
+    watchFiles: ['src/**', 'static/**'],
+    static:
+    {
+      watch: true,
+      directory: path.join(__dirname, '../static')
+    },
+    client:
+    {
+      logging: 'none',
+      overlay: true,
+      progress: false
+    },
+    setupMiddlewares: function (middlewares, devServer) {
+      console.log('------------------------------------------------------------')
+      console.log(devServer.options.host)
+      const port = devServer.options.port
+      const https = devServer.options.https ? 's' : ''
+      const domain1 = `http${https}://${devServer.options.host}:${port}`
+      const domain2 = `http${https}://localhost:${port}`
+
+      console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
+
+      return middlewares
+    }
+  }
 };
